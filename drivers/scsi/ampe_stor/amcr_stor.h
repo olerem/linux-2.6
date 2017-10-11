040aa7fceed94119fc39124616b111acf078de98
Oleksij Rempel <linux@rempel-privat.de>
2017-10-11 13:02:18 +0200

    m drivers/scsi/ampe_stor/amcr_stor.h
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- drivers/scsi/ampe_stor/amcr_stor.h
+++ drivers/scsi/ampe_stor/amcr_stor.h
@@ -53,47 +53,8 @@
 #include <scsi/scsi_eh.h>
 #include <scsi/scsi_host.h>
 
-static inline void au6601_writeb(u8 b, volatile void __iomem *addr)
-{
-	pr_debug("> wb: 0x%02x 0x%x\n", (int)addr & 0xff, b);
-	writeb(b, addr);
-}
+#include "au6601_debug.h"
 
-static inline void au6601_writew(u16 b, volatile void __iomem *addr)
-{
-	pr_debug("> ww: 0x%02x 0x%x\n", (int)addr & 0xff, b);
-	writew(b, addr);
-}
-
-static inline void au6601_writel(u32 b, volatile void __iomem *addr)
-{
-	pr_debug("> wl: 0x%02x 0x%x\n", (int)addr & 0xff, b);
-	writel(b, addr);
-}
-
-static inline u8 au6601_readb(volatile void __iomem *addr)
-{
-	u8 val;
-	val = readb(addr);
-	pr_debug("< rb: 0x%02x 0x%x\n", (int)addr & 0xff, val);
-	return val;
-}
-
-static inline u16 au6601_readw(volatile void __iomem *addr)
-{
-	u16 val;
-	val = readw(addr);
-	pr_debug("< rw: 0x%02x 0x%x\n", (int)addr & 0xff, val);
-	return val;
-}
-
-static inline u32 au6601_readl(volatile void __iomem *addr)
-{
-	u32 val;
-	val = readl(addr);
-	pr_debug("< rl: 0x%02x 0x%x\n", (int)addr & 0xff, val);
-	return val;
-}
 
 #define CR_DRIVER_NAME		"ampe_stor"
 #define DRIVER_VERSION 		"v1.18.03"
