3de89f25f5b6ef54cded6a5008447b484fd66242
Oleksij Rempel <linux@rempel-privat.de>
2018-10-05 11:20:36 +0200

    m drivers/misc/cardreader/alcor_pci.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- drivers/misc/cardreader/alcor_pci.c
+++ drivers/misc/cardreader/alcor_pci.c
@@ -414,12 +414,11 @@ static void alcor_pci_remove(struct pci_dev *pdev)
 
 	pci_aspm_ctrl(priv, 1);
 
+	mfd_remove_devices(&pdev->dev);
 
   spin_lock(&alcor_pci_lock);
 	idr_remove(&alcor_pci_idr, priv->id);
 	spin_unlock(&alcor_pci_lock);
-
-	mfd_remove_devices(&pdev->dev);
 
   pci_release_regions(pdev);
 	pci_set_drvdata(pdev, NULL);
